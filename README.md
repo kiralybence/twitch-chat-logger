98cc7bd473d2b25b38b766c6649384295cefcd04
Bence Király <kiralybencepecs@gmail.com>
2021-08-25 00:23:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Twitch Chat Logger
+
+A simple script that logs Twitch chat messages to `.txt` files.
+
+## Installation
+
+### Prerequisites
+- Node v12+
+
+### Setup
+- Install npm packages
+```bash
+npm install
+```
+
+- Create a `channels.txt` file with the channels you want to log (1 channel/row)
+```
+channel1
+channel2
+channel3
+```
+
+- Start the logger
+```bash
+npm start
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+const tmi = require('tmi.js')
+const fs = require('fs')
+
+const client = new tmi.client({
+    connection: {
+        reconnect: true,
+    },
+    channels: getChannels(),
+})
+
+client.connect()
+
+client.on('message', (channel, tags, message, self) => {
+    const timestamp = new Date(parseInt(tags['tmi-sent-ts'])).toISOString()
+    const date = timestamp.slice(0, 10)
+    const time = timestamp.slice(11, 19)
+    const logDir = `./logs/${channel}`
+
+    if (!fs.existsSync(logDir)) {
+        fs.mkdirSync(logDir, { recursive: true })
+    }
+
+    fs.appendFileSync(
+        `${logDir}/${date}.txt`,
+        `[${time}] ${tags['display-name']}: ${message}\n`
+    )
+})
+
+function getChannels() {
+    const channelPath = './channels.txt'
+
+    if (!fs.existsSync(channelPath)) {
+        fs.writeFileSync(channelPath, '')
+    }
+
+    return fs.readFileSync(channelPath, { encoding: 'utf8', flag: 'r' })
+        .replace(/\r\n/g,'\n')
+        .split('\n')
+        .map(channel => '#' + channel)
+}
