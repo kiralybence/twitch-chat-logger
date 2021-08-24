dd22913b0a9abb502b4c41188d4afa78c68a639a
Bence Király <kiralybencepecs@gmail.com>
2021-08-25 01:53:03 +0200

    Added queue to minimize disk writes

--- index.js
+++ index.js
@@ -10,21 +10,40 @@ const client = new tmi.client({
 
 client.connect()
 
-client.on('message', (channel, tags, message, self) => {
-    const timestamp = new Date(parseInt(tags['tmi-sent-ts'])).toISOString()
-    const date = timestamp.slice(0, 10)
-    const time = timestamp.slice(11, 19)
-    const logDir = `./logs/${channel}`
+let queue = {}
 
-    if (!fs.existsSync(logDir)) {
-        fs.mkdirSync(logDir, { recursive: true })
+client.on('message', (channel, tags, message, self) => {
+    const datetime = new Date(parseInt(tags['tmi-sent-ts'])).toISOString()
+    const date = datetime.slice(0, 10)
+    const time = datetime.slice(11, 19)
+
+    if (!queue[channel]) {
+        queue[channel] = {}
     }
 
-    fs.appendFileSync(
-        `${logDir}/${date}.txt`,
-        `[${time}] ${tags['display-name']}: ${message}\n`
-    )
+    if (!queue[channel][date]) {
+        queue[channel][date] = []
+    }
+
+    // TODO: escape \n (and other stuff) in messages
+    queue[channel][date].push(`[${time}] ${tags['display-name']}: ${message}\n`)
 })
+
+setInterval(() => {
+    for (const [channel, dates] of Object.entries(queue)) {
+        for (const [date, messages] of Object.entries(dates)) {
+            const logDir = `./logs/${channel}`
+
+            if (!fs.existsSync(logDir)) {
+                fs.mkdirSync(logDir, { recursive: true })
+            }
+
+            fs.appendFileSync(`${logDir}/${date}.txt`, messages.join(''))
+
+            delete queue[channel][date]
+        }
+    }
+}, 10000)
 
 function getChannels() {
     const channelPath = './channels.txt'
